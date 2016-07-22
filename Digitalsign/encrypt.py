8852623594ee9774df86db980849cbca47b879f7
Sudeep <sudeepbiswas02@gmail.com>
2016-07-22 02:47:54 +0000

    Digital Signature final with output

--- Digitalsign/encrypt.py
+++ Digitalsign/encrypt.py
@@ -3,24 +3,40 @@ import hashlib
 
 def genhash(fname):
 	#fname = input("Please enter file name : ")
-	fd = open(fname,"r")
-	org_hash = hashlib.sha512(fd.read()).hexdigest()
+	fd = open(fname,'r')
+	org_hash = hashlib.sha256((fd.read()).encode('utf-8')).hexdigest()
 	fd.close()
 	return org_hash
 
 def encrypt(h):
-	return
+	en = ""
+	for i in h:
+		if i=="":
+			en = en + (i)
+		else:
+			en = en + chr(ord(i) + 10)
+
+	return en
 
 
-fname = input("Please enter file name : ")
-org_hash = genhash(fname)
+def main():
 
-sign = open("dsign","w")
+	print("======Encryptor======")
+	fname = input("Please enter file name : ")
+	org_hash = genhash(fname)
+	
+	print("Hash value : " + str(org_hash))
 
-sign.write("Digital signature\n")
-sign.write(str(fname)+"\n")
-sign.write(str(org_hash+"\n"))
+	sign = open("dsign","w")
 
-sign.close()
+	#sign.write("Digital signature\n")
+	sign.write(fname)
 
+	en = encrypt(org_hash)
+
+	sign.write("\n")
+	sign.write(en)
+	sign.close()
+
+main()
 
--- Digitalsign/verify.py
+++ Digitalsign/verify.py
@@ -3,24 +3,36 @@ import hashlib
 
 def genhash(fname):
 	fd = open(fname,'r')
-	org_hash = hashlib.sha512(fd.read()).hexdigest()
+	org_hash = hashlib.sha256((fd.read()).encode('utf-8')).hexdigest()
 	fd.close()
 	return org_hash
 
-fname = raw_input("Please enter file name : ")
-#org_hash = genhash(fname)
+def decrypt(temp):
+	data = ""
+	for i in temp:
+		if i=="":
+			data = data + ""
+		else:
+			data = data + chr(ord(i)-10)
+	return data
 
-sigfile = open("dsign","r")
-temp = sigfile.readline().split()
-temp = sigfile.readline().split()
-print("Verifying digital signature for "+str(temp))
+def main():
 
-new_hash = genhash(temp)
+	#fname = input("Please enter file name : ")
+	#new_hash = genhash(fname)
 
-temp = sigfile.read.splitlines()
+	sigfile = open("dsign","r")
+	l  = sigfile.readlines()
+	temp = l[0].split("\n",1)[0]
+	new_hash = genhash(temp)
+	decr_hash = decrypt(l[1])
 
-if temp==new_hash:
-	print("Digital signature verified successfully\n")
-else:
-	print("Digital signature incorrect. File is tampered\n")
+	if decr_hash==new_hash:
+		print("Digital signature verified successfully\n")
+		print("Hash value : " + str(new_hash))
+	else:
+		print("Digital signature incorrect. File is tampered\n")
+		print("Original hash value : " + str(decr_hash)+ "\n")
+		print("Generated hash value : " + str(new_hash) + "\n")
 
+main()
